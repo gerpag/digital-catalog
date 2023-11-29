1f3e1bf0bca15989b3464264fd3a5d31896c5b08
alexis <as.alexismartinez@gmail.com>
2023-11-29 12:05:34 -0300

    feat: add tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-blue-600 h-screen flex justify-center items-center">
+      <h1 className="text-yellow-500 text-[100px]">
+        Bocaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, Bocaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+        Bocaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      </h1>
+    </div>
+  );
+};
+
+export default App;

c5866fc107d92a99b8d745acd682f05695b4ac60
gerpag <germinalfp@gmail.com>
2023-12-01 00:30:18 +0100

    form v1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
 import React from "react";
+import ProductManage from "./components/ProductManage";
 
 const App = () => {
   return (
-    <div className="bg-blue-600 h-screen flex justify-center items-center">
+    <>
+     <div className="bg-blue-600 h-screen flex justify-center items-center">
       <h1 className="text-yellow-500 text-[100px]">
         Bocaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, Bocaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
         Bocaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
       </h1>
     </div>
+    <ProductManage/>
+    </>
+   
   );
 };
 

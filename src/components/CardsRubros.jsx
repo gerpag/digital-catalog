e60b45dd0ca13cc910ef5ddc1ef84ff57cb6a00a
Micaela <micaelauvilla9@gmail.com>
2023-12-02 11:53:51 -0300

    feat: continue to view rubros

---
+++ src/components/CardsRubros.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const CardsRubros = () => {
+  return (
+    <div className="grid grid-cols-3 gap-3 ">
+      <img
+        className=" w-[470px] h-[350px] rounded-lg "
+        src="../../public/lamparas.jpg"
+        alt="imagen de aire libre"
+      />
+      <img
+        className="w-[470px] h-[350px] rounded-lg"
+        src="../../public/lamparas.jpg"
+        alt="imagen de aire libre"
+      />
+      <img
+        className="w-[470px] h-[350px] rounded-lg"
+        src="../../public/lamparas.jpg"
+        alt="imagen de aire libre"
+      />
+      <img
+        className="w-[470px] h-[350px] rounded-lg"
+        src="../../public/lamparas.jpg"
+        alt="imagen de aire libre"
+      />
+      <img
+        className="w-[470px] h-[350px] rounded-lg"
+        src="../../public/lamparas.jpg"
+        alt="imagen de aire libre"
+      />
+      <img
+        className="w-[470px] h-[350px] rounded-lg"
+        src="../../public/lamparas.jpg"
+        alt="imagen de aire libre"
+      />
+      <img
+        className="w-[470px] h-[350px] rounded-lg"
+        src="../../public/lamparas.jpg"
+        alt="imagen de aire libre"
+      />
+    </div>
+  );
+};
+
+export default CardsRubros;

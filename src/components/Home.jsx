8361979eb55501dd4de9bd3c17993f1f2680abf7
FranMack <franmack556@gmail.com>
2023-12-07 15:39:19 -0300

    break point home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home(){
 
     return(<>
    
-        <div className="w-[85vw] grid grid-cols-4 gap-3 mx-[auto] pt-[17vh] ">
+        <div className="w-[85vw] grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-1 xs:w-3/4 gap-3 mx-[auto] pt-[17vh]  ">
     {rubros.map((categoria,i)=>{
        return (
     <CardCategory categoria={categoria}/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import poliRubroLogo from "/polirubro_logo.png";
 import { Link, useLocation } from "react-router-dom";
+import {FaBars } from "react-icons/fa";
+
 
 const NavBar = ({ isAdminLoggedIn }) => {
   const location = useLocation();
   const links = ["INICIO", "RUBROS", "INFO", "CONTACTO", "AGREGAR","LOGIN"];
 
+  const [screenWidth,setScreenWidth]=useState(window.innerWidth)
+
+
+  const handleScreenWidth=()=>{
+    setScreenWidth(window.innerWidth)
+  }
+
+  window.addEventListener("resize",handleScreenWidth)
+
+
   return (
-    <div className=" box-border flex w-[100vw] h-[16vh]  fixed z-50 justify-center bg-[#f2f2f2] ">
-      <div class="flex h-[100%] w-[85%] justify-start">
+    <>
+    <div className="  flex w-[100vw] h-[16vh]  fixed z-50 justify-center bg-[#f2f2f2] items-center ">
+
+      {screenWidth>=1024 ? 
+       
+      (<div class="flex h-[100%] w-[85%] justify-start">
       
       <div className="h-full flex items-center ">
         <img
@@ -31,10 +47,23 @@ const NavBar = ({ isAdminLoggedIn }) => {
               </Link>
             </li>
           ))}
+         
         </ul>
       </div>
+      </div>):(<div class="flex h-[70%] w-[85%] justify-between items-center">   
+        <div className="h-full flex items-center ml-2% ">
+        <img
+          src={poliRubroLogo}
+          alt="Logo Polirubro"
+          className="h-[80%]"
+        />
       </div>
+
+      <FaBars style={{fontSize:"1.5rem", marginRight:"2%"}}/>
+
+         </div>)}
     </div>
+    </>
   );
 };
 

870fe7205abda430ad8ca1cea642567dfaef4dbe
gerpag <germinalfp@gmail.com>
2023-12-08 01:08:26 +0100

    drop options added

--- src/components/ProductManage.jsx
+++ src/components/ProductManage.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
 const ProductManage = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [subCategories, setSubCategories] = useState([]);
+
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -20,6 +25,80 @@ const ProductManage = () => {
     }
   };
 
+
+  const categories = [
+  "AIRE_LIBRE",
+  "ALFOMBRAS",
+  "BAZAR",
+  "CESTERIA",
+  "COMERCIAL",
+  "DE_MANO",
+  "DECO",
+  "DEPORTES",
+  "ESCOLAR",
+  "OFICINA",
+  "HOGAR_Y_JARDIN",
+  "INFANTIL",
+  "LAMPARAS",
+  " LECTURA_Y_MUSICA",
+  "MARROQUINERIA",
+  "PARED",
+  "VIA_PUBLICA",
+  "RELIGIOSO",
+  "RELOJES",
+  "SALUD",
+  "TECNO",
+  "TELEFONOS",
+  "TEXTIL",
+  "TOCADOR",
+];
+const subcategories = {
+  AIRE_LIBRE: ["PICNIC","CAMPING", "PLAYA"],
+  ALFOMBRAS: [],
+  BAZAR: ["BAR","BOTELLAS", "JARRAS", "CUBIERTOS","UTENSILLOS", "CONDIMENTEROS",
+    "ELECTRO (TOSTADORAS, LICUADORAS, JUGUERAS, CAFETERA ELECTRICA, MULTIPRO)",
+    "FRASCOS / LATAS",
+    "FRUTERAS / PANERAS", 
+    "FUENTES / BANDEJAS", "INFUSIONES (TE MATE CAFE, DESAYUNO)", 
+    "OLLAS / SARTENES",
+    "TABLAS / APOYAS",
+    "TERMOS", 
+    "VAJILLA",
+    "VASOS / COPAS",
+    "VARIOS BAZAR",
+    "PRODUCTOS (packagins)"
+    ],
+  CESTERIA: [],
+  COMERCIAL: [],
+  DE_MANO: ["AGENDAS / LIBRETAS", "ANTEOJOS", "BILLETERAS / MONEDEROS", "BIJOU", "BOLSILLO / CARTERA(polveras, espejitos, guantes, pañuelos, pastilleros, peines, algún maquillaje, abanicos)","DINERO / DOCUMENTOS", "LLAVES / LLAVEROS / CANDADOS","NECESEURS", "PARAGUAS", "TABACO (CIGARILLOS, MECHEROS, cigarreras, ETC)","VIAJE (automovil, mapas, pasajes aeropuerto, etc)"],
+  DECO: ["CAJAS / COFRES", "CENICEROS", "FLOREROS / JARRONES", "NAVIDAD . COTILLON", "OBJETOS", "PLATOS / BANDEJAS", "PORTARETRATOS", "SOUVENIRS / COLECCIONES", "VELAS / CANDELABROS"],
+  DEPORTES: [],
+  ESCOLAR: ["AULA", "CARPETAS Y CUADERNOS", "MOCHILAS / PORTAFOLIOS", "UTILES"],
+  OFICINA: ["ARCHIVO","MESA", "PAPELERIA", "TECNO (compus, maquina escribir, calculadoras)"],
+  HOGAR_Y_JARDIN: [],
+  INFANTIL: ["DECORACION", "JUGUETES", "JUEGOS DE MESA", "INSTRUMENTOS", "MUÑECOS / PELUCHES / TITERES","SILLAS / RODADOS"],
+  LAMPARAS: ["MESA", "TECHO", "PARED"],
+  LECTURA_Y_MUSICA: ['LECTURA Y MUSICA Subcategory1', 'LECTURA Y MUSICA Subcategory2'],
+  MARROQUINERIA: ["BOLSOS / MOCHILAS", "CARTERAS","ESCOLAR / INFANTIL", "EQUIPAJE", "PORTAFOLIOS"],
+  PARED: ["CUADROS", "DIPLOMAS / CALENDARIOS","ESPEJOS", "MASCARAS", "MAPAS / PLANOS", "PLACAS / CARTELERIA", "PLATOS", "TAPICES"],
+  VIA_PUBLICA: ["PATENTES", "ESCUDOS", "SEÑALETICA"],
+  RELIGIOSO: [],
+  RELOJES: ["MESA", "PARED"],
+  SALUD: ["MEDICINA", "FARMACIA"],
+  TECNO: ["ELECTRODOMÉSTICOS", "AUDIO", "TECNOLOGÍA"],
+  TELEFONOS: [],
+  TEXTIL: [],
+  TOCADOR: ["ASEO", "COSMETICA", "FRASCOS / PERFUMES", "HOMBRE", "PELUQUERÍA", "VARIOS TOCADOR o BAÑO"],
+};
+
+  const handleCategoryChange = (event) => {
+    const selectedCategory = event.target.value;
+    const categorySubcategories = subcategories[selectedCategory] || [];
+    
+    setSelectedCategory(selectedCategory);
+    setSubCategories(categorySubcategories.length > 0 ? categorySubcategories : ["Ninguna"]);
+  };
+
   return (
     <div className="flex items-center justify-center pt-[17vh]">
       <form encType="multipart/form-data" onSubmit={handleSubmit} className="bg-white p-8 shadow-md rounded max-w-md w-full">
@@ -35,28 +114,54 @@ const ProductManage = () => {
           />
         </div>
 
-        <div className="mb-3 flex items-center">
-          <label htmlFor="category" className="w-1/3 pr-4 text-right">
-            Categoría:
-          </label>
-          <input
-            type="text"
-            name="category"
-            className="w-2/3 p-2 border border-gray-300 rounded"
-          />
-        </div>
 
+        <div className="mb-3 flex items-center">
+        <label htmlFor="category" className="w-1/3 pr-4 text-right">
+          Categoría:
+        </label>
+        <select
+          name="category"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+          className="w-2/3 p-2 border border-gray-300 rounded"
+        >
+          
+          
+          {!selectedCategory && (
+            <option value="" disabled>Selecciona una categoría</option>
+          )}
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {selectedCategory && (
         <div className="mb-3 flex items-center">
           <label htmlFor="subCategory" className="w-1/3 pr-4 text-right">
             Sub-categoría:
           </label>
-          <input
-            type="text"
+          <select
             name="subCategory"
             className="w-2/3 p-2 border border-gray-300 rounded"
-          />
+          >
+          
+            {!subCategories.includes("Ninguna") && (
+              <option value="" >Selecciona una subcategoría</option>
+            )}
+            {subCategories.map((subCategory) => 
+            (
+              <option key={subCategory} value={subCategory}>
+                {subCategory}
+              </option>
+            ))}
+          </select>
         </div>
+      )}
 
+      
         <div className="mb-3 flex items-center">
           <label htmlFor="name" className="w-1/3 pr-4 text-right">
             Nombre de artículo:

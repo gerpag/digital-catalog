7bd2d8cb5ecbf920f00f94ba71c9a25534c00c5e
FranMack <franmack556@gmail.com>
2023-12-08 12:27:39 -0300

    modal with transition

--- src/commons/HamburgerMenu.jsx
+++ src/commons/HamburgerMenu.jsx
@@ -1,34 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 import poliRubroLogo from "/polirubro_logo.png";
 
+function HamburguerMenu({ handleModal, modal }) {
+  const location = useLocation();
+  const links = ["INICIO", "RUBROS", "INFO", "CONTACTO", "AGREGAR", "LOGIN"];
 
-function HamburguerMenu({handleModal}){
+  const [activo,setActivo]=useState(false);
 
-    const location = useLocation();
-    const links = ["INICIO", "RUBROS", "INFO", "CONTACTO", "AGREGAR","LOGIN"];
+  const handleActivo=()=>{
+    if(!activo){setActivo(!activo)}
+    
+    if(activo){
+      setActivo(!activo)
+      setTimeout(()=>{handleModal()},1000)
+    }
+  }
 
-    return(
-        <div className="w-[60vw] h-[100vh] top[17vh] right-[0vw] fixed z-50 bg-[#f2f2f2] border-[2px] border-[grey]" >
+  useEffect(()=>{
+    handleActivo()
+  },[modal])
+  
 
-<div onClick={handleModal} className=" w-full flex justify-start mt-[2%]"><FaTimes  style={{marginLeft:"5%",fontSize:"1.5rem"}}/></div>
-            
-
-            <div className="h-[15vh] w-full flex items-center justify-center ml-2%  border-b-[1px] border-[grey]">
-        <img
-          src={poliRubroLogo}
-          alt="Logo Polirubro"
-          className="h-[75%]"
-        />
+  return (
+    <div
+    className={`${
+      activo ? "translate-x-0" :"translate-x-full"
+    } fixed w-[60vw] h-[100vh] top[17vh]  z-50 bg-[#f2f2f2] border-[2px] border-[grey] right-0 ease-in-out transform transition-transform duration-1000`}
+  >
+      <div onClick={handleActivo} className="w-full flex justify-start mt-[2%]">
+        <FaTimes style={{ marginLeft: "5%", fontSize: "1.5rem" }} />
       </div>
 
-    <ul className="flex flex-col list-none w-[100%] justify-around items-center  h-1/2 mt-[5vh]">
-          {links.map((item, index) => (
-            <div key={index} className="w-[80%]">
-            <li  className="text-[1.1rem] text-center ">
+      <div className="h-[15vh] w-full flex items-center justify-center ml-2% border-b-[1px] border-[grey]">
+        <img src={poliRubroLogo} alt="Logo Polirubro" className="h-[75%]" />
+      </div>
+
+      <ul className="flex flex-col list-none w-[100%] justify-around items-center h-1/2 mt-[5vh]">
+        {links.map((item, index) => (
+          <div key={index} className="w-[80%]">
+            <li className="text-[1.1rem] text-center ">
               <Link
-                to={item==="INICIO"? "/":`/${item.toLocaleLowerCase()}`}
+                to={item === "INICIO" ? "/" : `/${item.toLocaleLowerCase()}`}
                 className={`hover:font-bold ${
                   location.pathname === `/${item.toLocaleLowerCase()}` ? "font-bold" : ""
                 }`}
@@ -36,14 +50,11 @@ function HamburguerMenu({handleModal}){
                 {item}
               </Link>
             </li>
-            </div>
-          ))}
-         
-        </ul>
-
-        
-
-        </div>
-    )
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 }
-export default HamburguerMenu
+
+export default HamburguerMenu;

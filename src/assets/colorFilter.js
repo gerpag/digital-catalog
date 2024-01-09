0ab52d7bd4277bb8ef38614d87bc1185ebd723b8
Micaela <micaelauvilla9@gmail.com>
2024-01-09 12:58:55 -0300

    organize rubros file

---
+++ src/assets/colorFilter.js
@@ -0,0 +1,24 @@
+export const colorsOne = [
+  { titulo: "amarillo", color: "#FFFF00" },
+  { titulo: "verde", color: "#008000" },
+  { titulo: "turquesa", color: "#40E0D0" },
+  { titulo: "celeste", color: "#B0E0E6" },
+  { titulo: "azul", color: "#314B98" },
+  { titulo: "naranja", color: "#FFA500" },
+  { titulo: "rojo", color: "#B7352D" },
+  { titulo: "bordó", color: "#800000" },
+  { titulo: "violeta", color: "#800080" },
+  { titulo: "rosa", color: "#FFC0CB" },
+];
+export const colorsTwo = [
+  { titulo: "blanco", color: "#FFFFFF" },
+  { titulo: "crudo", color: "#DCD0C0" },
+  { titulo: "beige", color: "#F5F5DC" },
+  { titulo: "marrón (ladrillo/madera)", color: "#8B4513" },
+  { titulo: "plateado", color: "#C0C0C0" },
+  { titulo: "dorado", color: "#FFD700" },
+  { titulo: "negro", color: "#000000" },
+  { titulo: "gris", color: "#808080" },
+  { titulo: "transparente", color: "#000000" },
+  { titulo: "multicolor", color: "#000000" },
+];

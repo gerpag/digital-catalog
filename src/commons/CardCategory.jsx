eb1c469cc61ae462911dbf3225b2c0a0dd0ba250
gerpag <germinalfp@gmail.com>
2024-03-03 23:27:15 +0100

    fix: categories's name unified

--- src/commons/CardCategory.jsx
+++ src/commons/CardCategory.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function CardCategory({ categoria }) {
+function CardCategory({ category }) {
+  if (!category) {
+    return null; 
+  }
   return (
-    <Link to={`/rubros/?cat=${categoria.categoria}`}>
+    <Link to={`/rubros/?cat=${category.category}`}>
       <div className="aspect-square relative  overflow-hidden  ">
         <p className="text-[white]  lg:text-[1.7rem] md:text-[1.4rem] xs:text-[1.7rem]  relative top-[42%] text-center font-sans font-semibold z-10">
-          {categoria.categoria}
+          {category.category}
         </p>
         <img
-          src={categoria.url_img}
+          src={category.url_img}
           className="object-fit object-left-bottom  absolute inset-0 hover:scale-[1.2] transition-transform duration-1000 hover:opacity-70 transition-filter hover:brightness-50"
+          alt={category.category}
         />
       </div>
     </Link>
